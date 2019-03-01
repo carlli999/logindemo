944fcea16add59c594a1487cc0ae5bbb846e900f
System Administrator <root@CarlLis-MacBook-Air.local>
2019-02-28 23:55:51 -0500

    first commit

---
+++ api/config/database.php
@@ -0,0 +1,28 @@
+<?php
+class Database{
+ 
+    // specify your own database credentials
+    private $host = "localhost";
+    private $port = "3308";
+    private $db_name = "PHPLearning";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";port=" . $this->port. ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+    ///opt/lampp/var/mysql/mysql.sock
+}
+?>
